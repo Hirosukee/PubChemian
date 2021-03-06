28fff97d72134e83374e4ff71db54061365f6e4c
Hirosuke <senolynn7584@gmail.com>
2021-03-06 15:40:01 +0900

    リファクタリング

--- cogs/help.py
+++ cogs/help.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from textwrap import dedent
 
 class Help(commands.Cog):
     def __init__(self, bot):
@@ -7,22 +8,20 @@ class Help(commands.Cog):
 
     @commands.command(name="help", aliases=["h"])
     async def _help(self, ctx):
-        help_txt = """
-__**PubChemian Commands**__
+        await ctx.send(dedent("""\
+        __**PubChemian Commands**__
 
-> `.search[.s] <keyword> <*args>`
-To search compounds.
-`-translate[-t]` to translate a keyword to english.
-`-limit[-l] <amount>` to limit to search.
-`-[smiles, cid, formula, name]` to change a search type.
-`-image[-i]` to show structural formula as bigger.
-`-thumbnail[-tb]` to show structural formula as smaller.
+        > `.search[.s] <keyword> (<*args>)`
+        To search compounds.
+        `-translate[-t]` to translate a keyword to english.
+        `-limit[-l] <amount>` to limit to search.
+        `-[smiles, cid, formula, name]` to change a search type.
+        `-image[-i]` to show structural formula as bigger.
+        `-thumbnail[-tb]` to show structural formula as smaller.
 
-> `.help[.h]`
-To show this help.
-        """
-
-        await ctx.send(help_txt)
+        > `.help[.h]`
+        To show this help.
+        """))
 
 def setup(bot):
     bot.add_cog(Help(bot))
