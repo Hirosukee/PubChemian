f0afb639e531fd2ba471cef9358b682d89197f4d
Hirosuke <senolynn7584@gmail.com>
2021-03-06 13:52:59 +0900

    inline

--- cogs/search.py
+++ cogs/search.py
@@ -50,12 +50,12 @@ class Search(commands.Cog):
 
             #embed
             e = discord.Embed(title=elem.synonyms[0], url=f"https://pubchem.ncbi.nlm.nih.gov/compound/{elem.cid}", description="")
-            e.add_field(name="Formula", value=elem.molecular_formula)
-            e.add_field(name="Weight", value=str(elem.molecular_weight))
-            e.add_field(name="CID", value=str(elem.cid))
+            e.add_field(name="Formula", value=elem.molecular_formula, inline=True)
+            e.add_field(name="Weight", value=str(elem.molecular_weight), inline=True)
+            e.add_field(name="CID", value=str(elem.cid), inline=True)
             e.add_field(name="IUPAC", value=str(elem.iupac_name), inline=False)
             e.add_field(name="Smile", value=str(elem.isomeric_smiles), inline=False)
-            e.set_footer(text=str(elem.synonyms) if len(str(elem.synonyms)) <= 200 else str(elem.synonyms)[:200].replace("[", "").replace("]", "").replace("'", "") + "...")
+            e.set_footer(text=str(elem.synonyms) if len(str(elem.synonyms)) <= 70 else str(elem.synonyms)[:200].replace("[", "").replace("]", "").replace("'", "") + "...")
             e.set_thumbnail(url=f"https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid={elem.cid}&t=l")
 
             #send
